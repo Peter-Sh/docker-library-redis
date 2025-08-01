d1af322226c3a0c13d452dc295bb8762f1193fa2
Petar Shtuchkin <petar.shtuchkin@redis.com>
2025-08-01 19:54:30 +0300

    Auto merge from release branch

--- .github/actions/ensure-release-branch/ensure-release-branch.sh
+++ .github/actions/ensure-release-branch/ensure-release-branch.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-set -e
-#set -x
 
 # This script ensures that a release branch and release version branch exist for a given release tag.
 # It allow-modifys and pushes both branches if they do not exist.
 # It also checks out the release version branch at the end.
 # https://redislabs.atlassian.net/wiki/spaces/RED/pages/5293342875/Redis+OSS+release+automation
 
+set -e
+#set -x
 
 # shellcheck disable=SC2034
 last_cmd_stdout=""
@@ -60,21 +60,6 @@ RELEASE_VERSION_BRANCH="$TAG"
 echo "TAG: $TAG"
 echo "RELEASE_VERSION_BRANCH: $RELEASE_VERSION_BRANCH"
 
-# Check if RELEASE_VERSION_BRANCH exists in origin
-execute_command git ls-remote --heads origin "$RELEASE_VERSION_BRANCH"
-if echo "$last_cmd_stdout" | grep -q "$RELEASE_VERSION_BRANCH"; then
-    execute_command git fetch origin "$RELEASE_VERSION_BRANCH"
-    execute_command git checkout "$RELEASE_VERSION_BRANCH"
-    echo "Successfully checked out to $RELEASE_VERSION_BRANCH"
-    exit 0
-fi
-
-echo "Branch $RELEASE_VERSION_BRANCH does not exist in origin"
-if [ -z "$ALLOW_MODIFY" ]; then
-    echo "Refuse to modify repository without --allow-modify option"
-    exit 1
-fi
-
 # Detect RELEASE_BRANCH name (release/X.Y format)
 RELEASE_BRANCH="release/$(echo "$TAG" | grep -Po '^\d+\.\d+')"
 echo "RELEASE_BRANCH: $RELEASE_BRANCH"
@@ -84,9 +69,12 @@ execute_command git ls-remote --heads origin "$RELEASE_BRANCH"
 if echo "$last_cmd_stdout" | grep -q "$RELEASE_BRANCH"; then
     echo "Branch $RELEASE_BRANCH exists in origin"
     execute_command git fetch origin "$RELEASE_BRANCH"
-    execute_command git checkout "$RELEASE_BRANCH"
 else
     echo "Branch $RELEASE_BRANCH does not exist in origin, need to create it"
+    if [ -z "$ALLOW_MODIFY" ]; then
+        echo "Refuse to modify repository without --allow-modify option"
+        exit 1
+    fi
 
     # Detect base branch (previous existing branch for the version)
     MAJOR_MINOR=$(echo "$TAG" | grep -Po '^\d+\.\d+')
@@ -110,6 +98,61 @@ else
     echo "Created and pushed $RELEASE_BRANCH based on $BASE_BRANCH"
 fi
 
+# Check if RELEASE_VERSION_BRANCH exists in origin
+execute_command git ls-remote --heads origin "$RELEASE_VERSION_BRANCH"
+if echo "$last_cmd_stdout" | grep -q "$RELEASE_VERSION_BRANCH"; then
+    execute_command git fetch origin "$RELEASE_VERSION_BRANCH"
+    execute_command git checkout "$RELEASE_VERSION_BRANCH"
+    echo "Successfully checked out to $RELEASE_VERSION_BRANCH"
+
+    # Check if there are changes in release branch that are not in release version branch
+    echo "Checking for differences between $RELEASE_BRANCH and $RELEASE_VERSION_BRANCH..."
+    execute_command git fetch origin "$RELEASE_BRANCH"
+
+    # Compare the two branches to see if there are commits in release branch not in release version branch
+    execute_command git rev-list --count "origin/$RELEASE_VERSION_BRANCH..origin/$RELEASE_BRANCH"
+    COMMITS_BEHIND=$(echo "$last_cmd_stdout" | tr -d '[:space:]')
+
+    if [ "$COMMITS_BEHIND" -gt 0 ]; then
+        echo "Found $COMMITS_BEHIND commit(s) in $RELEASE_BRANCH that are not in $RELEASE_VERSION_BRANCH"
+
+        if [ -z "$ALLOW_MODIFY" ]; then
+            echo "Changes detected but refusing to merge without --allow-modify option"
+            exit 1
+        fi
+
+        echo "Attempting to merge changes from $RELEASE_BRANCH into $RELEASE_VERSION_BRANCH..."
+
+        # Try to merge the release branch into the current release version branch
+        execute_command git merge "origin/$RELEASE_BRANCH" --no-edit
+        if [ $last_cmd_result -eq 0 ]; then
+            echo "Successfully merged changes from $RELEASE_BRANCH"
+
+            # Push the merged changes to origin
+            execute_command git push origin "$RELEASE_VERSION_BRANCH"
+            if [ $last_cmd_result -eq 0 ]; then
+                echo "Successfully pushed merged changes to origin/$RELEASE_VERSION_BRANCH"
+            else
+                echo "Error: Failed to push merged changes to origin"
+                exit 1
+            fi
+        else
+            echo "Error: Merge failed. There may be conflicts that need manual resolution."
+            exit 1
+        fi
+    fi
+
+    exit 0
+
+fi
+
+echo "Branch $RELEASE_VERSION_BRANCH does not exist in origin"
+if [ -z "$ALLOW_MODIFY" ]; then
+    echo "Refuse to modify repository without --allow-modify option"
+    exit 1
+fi
+
+execute_command git checkout "$RELEASE_BRANCH"
 # At this point, we should be on RELEASE_BRANCH
 echo "Current branch: $(git branch --show-current)"
 
