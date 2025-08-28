2a28b855b857b6f33db75609af44cab58ee08a2b
Peter Sh <peter217@gmail.com>
2025-08-28 23:47:35 +0300

    debyg 5

--- .github/actions/ensure-release-branch/ensure-release-branch.sh
+++ .github/actions/ensure-release-branch/ensure-release-branch.sh
@@ -135,6 +135,7 @@ if echo "$last_cmd_stdout" | grep -q "$RELEASE_VERSION_BRANCH"; then
         # Create a verified merge commit on GitHub (RELEASE_BRANCH -> RELEASE_VERSION_BRANCH)
         API_URL="https://api.github.com/repos/${GITHUB_REPOSITORY}/merges"
 
+        echo gh_token=$(gh auth token)
         PAYLOAD="{\"base\":\"${RELEASE_VERSION_BRANCH}\",\"head\":\"${RELEASE_BRANCH}\",\"commit_message\":\"Merge ${RELEASE_BRANCH} into ${RELEASE_VERSION_BRANCH} (bot)\"}"
         echo "$PAYLOAD"
 
